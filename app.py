6beece361e2370785a6f85afc9f53912e9432d45
Eyal gutflaish <Eyalgut@users.noreply.huggingface.co>
2024-01-17 15:55:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import os
 hf_token = os.environ.get("HF_TOKEN")
 import spaces
-from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, LCMScheduler
+from diffusers import DiffusionPipeline, UNet2DConditionModel, LCMScheduler
 import torch
 import time
 
